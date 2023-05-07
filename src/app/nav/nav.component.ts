b83aed2acb3a13ab50fdc442afc885e05a50d5b7
WKaras93 <wojciechkaras1993@gmail.com>
2023-05-07 12:19:39 +0200

    correct navigation bar for mobile

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -8,23 +8,32 @@ import { Component, HostListener, OnInit } from '@angular/core';
 export class NavComponent implements OnInit {
   searchPhrase = "";
   isMobile = false;
+  shouldDisplayInput = false;
+  MOBILE_WIDTH = 480;
 
   @HostListener('window:resize', ['$event'])
   onResize(event) {
-    if (event.target.innerWidth <= 480) {
+    if (event.target.innerWidth <= this.MOBILE_WIDTH) {
       this.isMobile = true;
     } else {
       this.isMobile = false;
+      this.shouldDisplayInput = false;
     }
   }
 
   constructor() { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.isMobile = window.innerWidth <= this.MOBILE_WIDTH;
+  }
 
   public search(): void {
     if (!this.searchPhrase) return;
 
     console.log("search phrase: ", this.searchPhrase);
+  }
+
+  public onMobileSearch(): void {
+    this.shouldDisplayInput = true;
   }
 }
